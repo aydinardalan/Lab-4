5c70529e5ccbaff122405ad87cedbac46e8d0794
seymi176 <mehdi.mirshojaei@gmail.com>
2022-09-17 16:01:27 +0200

    Initial function body and related formula set up

--- R/LabFour-package.R
+++ R/LabFour-package.R
@@ -1,9 +1,22 @@
 #' @keywords internal
 "_PACKAGE"
 
-test <- function(){
-  return empty
+linreg <- function(var,data){
+  
+  #Formula
+  X <<- model.matrix(var, data)
+  y <<- as.matrix(data[all.vars(var)[1]])
+  Beta <<- solve(t(X)%*%X)%*%t(X)%*%y
+  y_fitted <<- X%*%Beta
+  e <<- y - y_fitted
+  n <<- length(y)
+  p <<- length(all.vars(var))
+  df <<- n - p -1
+  sigma_square <<- (t(e) %*% e)/df
+  var_B <<- as.numeric(sigma_square) * solve(t(X)%*%X)
+  t_value <<- Beta / as.numeric(sqrt(var(Beta)))
 }
+
 
 ## usethis namespace: start
 ## usethis namespace: end
