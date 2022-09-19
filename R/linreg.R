c76520cbd48da3214284ebceb3274b17a5e3b231
seymi176 <mehdi.mirshojaei@gmail.com>
2022-09-19 12:33:00 +0200

    function method

--- R/linreg.R
+++ R/linreg.R
@@ -18,5 +18,39 @@ linreg <- setRefClass("linreg",
                                     pval="matrix",
                                     Formula="formula",
                                     Data="character",
-                                    sqrtstresiduals="matrix")
+                                    sqrtstresiduals="matrix"),
+                      
+                      methods = list(
+                        # Constructor (Creating object)
+                        initialize=function(formula,data) {
+                          # Dependent and independent variables
+                          X <<- model.matrix(formula,data)
+                          Y <<- as.matrix(data[all.vars(formula)[1]])
+                          # Regressions Coefficients
+                          B <<- solve(t(X)%*% X)%*% t(X) %*% Y
+                          # Fitted values
+                          Yfit <<- X %*% B
+                          # Residuals
+                          E <<- Y-Yfit
+                          P <<- length(B)
+                          N <<- length(Y)
+                          # Degrees of freedom
+                          dof <<- N-P
+                          # The Residual Variance
+                          sigsq <<- as.vector(t(E)%*%E/dof)                                                  
+                          Estd <<- sqrt(sigsq) 
+                          # Variance of Regression Coefficients
+                          Xt <<- t(X)
+                          XtX <<- solve(Xt %*% X)
+                          var_B <<- sigsq * XtX   
+                          tval <<- B/as.numeric(sqrt(var(B)))
+                          # Standardized residuals for summary plot
+                          sqrtstresiduals <<- sqrt(abs(E/sd(E)))
+                          # p-values
+                          pval <<- 2 * pt(abs(tval), dof, lower.tail = FALSE)
+                          #Saving names
+                          Formula <<- formula
+                          Data <<- deparse(substitute(data))
+                        }
+)
 )
