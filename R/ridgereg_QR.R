30d6904d9a980add64644f9770784bb6c3931a17
aydinardalan <aydin.ardalan@gmail.com>
2022-10-23 12:42:40 +0200

    Adding QR

--- R/ridgereg_QR.R
+++ R/ridgereg_QR.R
@@ -1,23 +1,23 @@
-formula=Petal.Length~Species
-data=iris
-my_data_x = model.matrix(object = formula, data = data)
-y = data[[all.vars(formula)[[1]]]]
-my_normalized_data = scale(my_data_x)
-X = as.matrix(my_normalized_data)[,-1]
+#'Ridge Regression with QR decomposition
 
-#Beginning of QR decomposition calculations
+#' @importFrom MASS lm.ridge
 
-Xupdated = rbind(X, diag(rep(sqrt(lambda), ncol(X))))
-yupdated = c(y, rep(0, ncol(X)))
-QR = qr(Xupdated)
-X_Q = qr.Q(QR)
-X_R = qr.R(QR)
-coefs <<- drop(solve(X_R, (t(X_Q)%*%yupdated)))
-
-#End of QR decomposition calculations
-
-beta_zero = mean(y)
-
-y_est <<- drop(X %*% coefs + beta_zero) #calculates estimated y
-
-},
+ridgereg_QR = setRefClass(
+  Class = "ridgereg_QR",
+  fields = c(  "coefs",
+               "y_est",
+               "local_data_name",
+               "local_lambda"),
+  methods = list(
+    initialize = function(formula, data, lambda = 0){
+      
+      local_data_name <<- deparse(substitute(data))
+      local_lambda<<- lambda
+      
+      my_data_x = model.matrix(object = formula, data = data)
+      y = data[[all.vars(formula)[[1]]]]
+      my_normalized_data = scale(my_data_x)
+      X = as.matrix(my_normalized_data)[,-1]
+    }
+  )
+)
